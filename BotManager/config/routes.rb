4ca70c65e78075a4e8890181efc88f5a8ee59df3
Alizee Renault <melissa.grach@live.fr>
2021-10-12 16:48:57 +0200

    ~

--- BotManager/config/routes.rb
+++ BotManager/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#search'
   get '/' => 'home#search'
+  get '/all' => 'home#search'
   get '/keyword/:keyword' => 'home#keyword'
   get '/user/:user' => 'home#user'
   get '/alerter/:alerter' => 'home#alerter'
