c040c316023119d34d1498d77d5e12e5ae296bda
Lucas Sirignano <lucas.sirignano@gmail.com>
2022-11-24 23:16:30 +0100

    ~>

--- BotManager/app/controllers/api_controller.rb
+++ BotManager/app/controllers/api_controller.rb
@@ -5,8 +5,8 @@ class ApiController < ApplicationController
 		begin
 			Bot.add_one_tweet params[:tid]
 		rescue
-			render :nothing => true, status: 500 and return
+			head 500 and return
 		end
-		render :nothing => true, status: 200 and return
+		head 200 and return
 	end
 end
