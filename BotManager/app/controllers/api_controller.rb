f846eb5cf53a6d38a714701fab5acee7120c740b
Lucas Sirignano <lucas.sirignano@gmail.com>
2022-11-24 23:21:02 +0100

    ~>

--- BotManager/app/controllers/api_controller.rb
+++ BotManager/app/controllers/api_controller.rb
@@ -2,11 +2,11 @@ class ApiController < ApplicationController
 	protect_from_forgery except: :post_tweet
 
 	def post_tweet
-		begin
+		# begin
 			Bot.add_one_tweet params[:tid]
-		rescue
-			head 500 and return
-		end
+		# rescue
+		# 	head 500 and return
+		# end
 		head 200 and return
 	end
 end
