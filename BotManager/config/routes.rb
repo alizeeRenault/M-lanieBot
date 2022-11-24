589a777cc45745809a5781adf611565908c3d07a
Lucas Sirignano <lucas.sirignano@gmail.com>
2022-11-24 23:44:12 +0100

    ~>

--- BotManager/config/routes.rb
+++ BotManager/config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get '/stats' => 'home#stat'
   get '/delete' => 'home#delete'
   post '/api/post_tweet' => 'api#post_tweet'
+  get '/api/post_tweet' => 'api#post_tweet'
   delete '/delete/tweet' => 'home#delete_tweet'
   delete '/delete/user' => 'home#delete_user'
   delete '/add/tweet' => 'home#add_tweet'
