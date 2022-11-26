4bfb21e542587c6d777e90cd24df00c73b0cb189
Lucas Sirignano <lucas.sirignano@gmail.com>
2022-11-26 15:04:28 +0100

    ~

--- BotManager/config/initializers/twitter.rb
+++ BotManager/config/initializers/twitter.rb
@@ -1,13 +1,13 @@
 CLIENT = Twitter::REST::Client.new do |config|
-	config.consumer_key = "cpZk11aN9xdwyIRUkb1oItV7s"
-	config.consumer_secret = "2EJd0cAndo7kL6anSOlXVPghpSAYgYPEK2eLNaiNuF0AKTpxvd"
-	config.access_token = "1347605093651197952-ttIm4uUVU8o5RjHVKjfsnIlzFwjTWs"
-	config.access_token_secret = "dOPlMyHy96Edg43qNsWE2K1vCC6UdfX4SjBSrpJtRXbrL"
+	# config.consumer_key = "cpZk11aN9xdwyIRUkb1oItV7s"
+	# config.consumer_secret = "2EJd0cAndo7kL6anSOlXVPghpSAYgYPEK2eLNaiNuF0AKTpxvd"
+	# config.access_token = "1347605093651197952-ttIm4uUVU8o5RjHVKjfsnIlzFwjTWs"
+	# config.access_token_secret = "dOPlMyHy96Edg43qNsWE2K1vCC6UdfX4SjBSrpJtRXbrL"
 
-# 	config.consumer_key = "LB7HKGzrhIpiWcMFgdsJ0qai1"
-# 	config.consumer_secret = "mStGhkKxNuX1f0afgtuKo9sCyNSvJeNPC9CmOCBQ17AQj6o6Tb"
-# 	config.access_token = "1596091893045202944-4dXPv64jeyFK40XFOcsAPoEjUl8YrJ"
-# 	config.access_token_secret = "xCmkiFY9PSoCybdOhi1shno3MErNK5F9CGmKnHZWrhrii"
+	config.consumer_key = "EMPD12wNE8vr8BxEOiUJqyWRj"
+	config.consumer_secret = "LAijeoP44BocwtfTsKosZb0z7oqafj1ox5YtuNcJY9ZoP8niHR"
+	config.access_token = "1596091893045202944-iHUfltNsLj7Kgtpp5sjVHbfZ7kaBI0"
+	config.access_token_secret = "os3LK170SQSJw1yeoWGvChWoMuwqYtlzBw5jGMLzxJeGO"
 end
 #cpZk11aN9xdwyIRUkb1oItV7s APIKEY
 #2EJd0cAndo7kL6anSOlXVPghpSAYgYPEK2eLNaiNuF0AKTpxvd API secret
@@ -19,23 +19,23 @@ end
 
 # NEw
 # API Key
-# LB7HKGzrhIpiWcMFgdsJ0qai1
+# EMPD12wNE8vr8BxEOiUJqyWRj
 # API Key Secret
-# mStGhkKxNuX1f0afgtuKo9sCyNSvJeNPC9CmOCBQ17AQj6o6Tb
+# LAijeoP44BocwtfTsKosZb0z7oqafj1ox5YtuNcJY9ZoP8niHR
 # Bearer Token
-# AAAAAAAAAAAAAAAAAAAAANX2jgEAAAAAABCgm98z3I0UwpYTK0GFgfDuw90%3DkWZzMb6FE9T8sFbeWKO6fihyvs8AYpUQUmA0A3XUh321MO2bQ9
+# AAAAAAAAAAAAAAAAAAAAAN4JjwEAAAAArzLqx%2B9kpurz1YQ5ww0q0AdxwEk%3Dp2CdSktjqRZD3W7R8KEWzoKPP6Y7O3GMBU4uuDLjwsTPyXOTmH
 
 # Access Token
 
-# 1596091893045202944-4dXPv64jeyFK40XFOcsAPoEjUl8YrJ
+# 1596091893045202944-iHUfltNsLj7Kgtpp5sjVHbfZ7kaBI0
 
 # Access Token Secret
-# xCmkiFY9PSoCybdOhi1shno3MErNK5F9CGmKnHZWrhrii
+# os3LK170SQSJw1yeoWGvChWoMuwqYtlzBw5jGMLzxJeGO
 
-# 	config.consumer_key = "LB7HKGzrhIpiWcMFgdsJ0qai1"
-# 	config.consumer_secret = "mStGhkKxNuX1f0afgtuKo9sCyNSvJeNPC9CmOCBQ17AQj6o6Tb"
-# 	config.access_token = "1596091893045202944-4dXPv64jeyFK40XFOcsAPoEjUl8YrJ"
-# 	config.access_token_secret = "xCmkiFY9PSoCybdOhi1shno3MErNK5F9CGmKnHZWrhrii"
+# 	config.consumer_key = "EMPD12wNE8vr8BxEOiUJqyWRj"
+# 	config.consumer_secret = "LAijeoP44BocwtfTsKosZb0z7oqafj1ox5YtuNcJY9ZoP8niHR"
+# 	config.access_token = "1596091893045202944-iHUfltNsLj7Kgtpp5sjVHbfZ7kaBI0"
+# 	config.access_token_secret = "os3LK170SQSJw1yeoWGvChWoMuwqYtlzBw5jGMLzxJeGO"
 
 # old
 # 	config.consumer_key = "cpZk11aN9xdwyIRUkb1oItV7s"
